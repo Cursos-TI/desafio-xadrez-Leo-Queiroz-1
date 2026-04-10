7201ef2aaf898cbf96f9b7e47b52a45218646205
Leonardo de Queiroz <jrleo010906@gmail.com>
2026-04-10 00:06:22 +0000

    adicionado a movimentação nivel iniciante

--- xadrez.c
+++ xadrez.c
@@ -27,6 +27,30 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+   
+    int torre = 0;
+    int bispo = 0;
+    int rainha= 0;
+    //movimentando a torre
+    for(torre ; torre < 5 ; torre++){
+        printf("torre\n");
+        printf("direita\n");
+    }
+    printf("\n");
+    //movimentando o bispo
 
+    while(bispo < 5){
+        bispo++;
+        printf("bispo\n");
+        printf("cima,direita\n");
+
+    }
+        printf("\n");
+    //movimentando a Rainha
+    do{
+        rainha++;
+        printf("rainha\n");
+        printf("esquerda\n");
+    }while(rainha < 8);
     return 0;
 }
