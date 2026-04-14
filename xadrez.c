2042b1445dc29e79c127b30b5a19ff576dd88efd
Leonardo de Queiroz <jrleo010906@gmail.com>
2026-04-14 23:36:34 +0000

    Adicionado a movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -53,4 +53,15 @@ int main() {
         printf("esquerda\n");
     }while(rainha < 8);
     return 0;
+
+    //movimentando o cavalo
+    printf("\n");//identando uma linha vazia
+       int movimentacaoCavalo = 1;//valor inicial do cavalo
+
+       while(movimentacaoCavalo--){
+            for(int i = 0; i<2;i++){
+            printf("baixo\n");//imprime "Baixo 2 vezes"
+            }
+            printf("esquerda\n");
+       }
 }
