d89a1ab037e8fa8c8def2a2ae52752b9a6797931
Leonardo de Queiroz <jrleo010906@gmail.com>
2026-04-16 11:01:17 +0000

    implementado as movimentações usando recursão em todas as peças e loop aninhado com recursão no bispo

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -27,42 +27,61 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-   
-    int torre = 0;
-    int bispo = 0;
-    int rainha= 0;
-    //movimentando a torre
-    for(torre ; torre < 5 ; torre++){
-        printf("torre\n");
-        printf("direita\n");
+    //loops recursivos que movimenta as peças
+    void movimentoTorre(int movimento){
+        if(movimento > 0){
+            printf("torre\n");
+            printf("direita\n");
+            movimentoTorre(movimento - 1);
+        }
     }
+        void movimentoBispo(int movimento){
+            if(movimento > 0){
+                printf("Bispo\n");
+            
+            for (int i = 0 ; i < 1; i++){
+                for(int j = 0 ; j < 1; j++ ){
+                    printf("direita\n");
+                    printf("cima\n");
+                }
+                movimentoBispo(movimento - 1);
+            }
+           }
+        }
+    
+    void movimentoRainha(int movimento){
+        if(movimento > 0){
+            printf("Rainha\n");
+            printf("Esquerda\n");
+            movimentoRainha(movimento - 1);
+        }
+    }
+
+
+int main(){
+    //Váriaveis
+    int torre = 5;
+    int bispo = 5;
+    int rainha= 8;
+    //movimentando a torre
+    movimentoTorre(torre);
+
     printf("\n");
     //movimentando o bispo
-
-    while(bispo < 5){
-        bispo++;
-        printf("bispo\n");
-        printf("cima,direita\n");
-
-    }
+    movimentoBispo(bispo);
         printf("\n");
     //movimentando a Rainha
-    do{
-        rainha++;
-        printf("rainha\n");
-        printf("esquerda\n");
-    }while(rainha < 8);
-    
-
-    //movimentando o cavalo
-    printf("\n");//identando uma linha vazia
+    movimentoRainha(rainha);
+       //movimentando o cavalo
+       printf("\n");//identando uma linha vazia
        int movimentacaoCavalo = 1;//valor inicial do cavalo
 
        while(movimentacaoCavalo--){
-            for(int i = 0; i<2;i++){
-            printf("baixo\n");//imprime "Baixo 2 vezes"
+            for(int i = 0; i < 2;i++){
+            printf("cima\n");//imprime "Cima 2 vezes"
             }
-            printf("esquerda\n");
+            printf("direita\n");
        }
-       return 0;
+
+    return 0;
 }
